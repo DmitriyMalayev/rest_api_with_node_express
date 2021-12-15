4194e8f2f7f5b7ffcbc2ee08bd086dac12dc560e
Dmitriy Malayev <56614284+DmitriyMalayev@users.noreply.github.com>
2021-12-14 23:41:41 -0500

    added uuid

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,6 @@
 import express from "express";
+import { v4 as uuidv4 } from "uuid";
+uuid();
 
 const router = express.Router();
 
@@ -20,9 +22,13 @@ router.get("/", (req, res) => {
   res.send(users);
 });
 
-router.post("/", (req, res)=> {
-   
-})
+router.post("/", (req, res) => {
+  const user = req.body;
+  users.push(user);
+  res.send(
+    `User with the name ${user.firstName} has been added to the Database`
+  );
+});
 
 export default router;
 // All routes in this file already start with /users
