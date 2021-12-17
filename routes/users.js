b693015f77b6ab6ecf687974f61ea8eee93b34fe
Dmitriy Malayev <56614284+DmitriyMalayev@users.noreply.github.com>
2021-12-16 22:20:45 -0500

    delete route

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,5 @@
 import express from "express";
 import { v4 as uuidv4 } from "uuid";
-uuid();
 
 const router = express.Router();
 
@@ -24,11 +23,26 @@ router.get("/", (req, res) => {
 
 router.post("/", (req, res) => {
   const user = req.body;
-  users.push(user);
+
+  users.push({ ...user, id: uuidv4() });
+
+  // const userId = uuidv4();
+  // const userWithId = { ...user, id: userId };
+
   res.send(
     `User with the name ${user.firstName} has been added to the Database`
   );
 });
 
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const foundUser = users.find((user) => user.id === id);
+  res.send(foundUser);
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  users.filter((user) => user.id !== id);   //removes what returns false 
+});
 export default router;
 // All routes in this file already start with /users
